78ceb6fe7124f9fd00292f32b569de9471aac414
szadev <106595014+szadev@users.noreply.github.com>
2022-11-25 23:43:27 +0300

    Add textbox class with hint
    Textbox with hint class that inherits TextBox windows form class.

---
+++ supportclasses/TextBoxHint.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+public class TextBoxHint : TextBox
+{
+    string hint;
+    public string Hint
+    {
+        get { return hint; }
+        set { hint = value; Invalidate(); }
+    }
+    protected override void WndProc(ref Message m)
+    {
+        base.WndProc(ref m);
+        if (m.Msg == 0xf)
+        {
+            if (!Focused && string.IsNullOrEmpty(Text)
+                && !string.IsNullOrEmpty(Hint))
+            {
+                using (var g = CreateGraphics())
+                {
+                    TextRenderer.DrawText(g, Hint, Font,
+                        ClientRectangle, SystemColors.GrayText, BackColor,
+                        TextFormatFlags.Top | TextFormatFlags.Left);
+                }
+            }
+        }
+    }
+}
